2ea25a68ec04db2aa916f95aa2fb7f195691a710
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-06-13 13:20:20 +0200

    feat(gameplay): add Tetromino class and basic board logic to GameView

--- app/src/main/java/es/nellagames/tetris/GameView.java
+++ app/src/main/java/es/nellagames/tetris/GameView.java
@@ -1,4 +1,43 @@
 package es.nellagames.tetris;
 
-public class GameView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View {
+    private final Paint paint = new Paint();
+
+    private final int[][] board = new int[20][10];
+    private Tetromino currentTetromino;
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        spawnTetromino(); // Llamar a spawnTetromino en el constructor una sola vez
+    }
+
+    private void spawnTetromino() {
+        int type = (int) (Math.random() * Tetromino.SHAPES.length);
+        currentTetromino = new Tetromino(type);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        paint.setColor(Color.LTGRAY);
+        int cellSize = getWidth() / 10;
+
+        // Dibujar líneas horizontales
+        for (int i = 0; i <= 20; i++) {
+            canvas.drawLine(0, i * cellSize, getWidth(), i * cellSize, paint);
+        }
+
+        // Dibujar líneas verticales
+        for (int i = 0; i <= 10; i++) {
+            canvas.drawLine(i * cellSize, 0, i * cellSize, getHeight(), paint);
+        }
+    }
 }
--- app/src/main/java/es/nellagames/tetris/Tetromino.java
+++ app/src/main/java/es/nellagames/tetris/Tetromino.java
@@ -1,4 +1,35 @@
 package es.nellagames.tetris;
 
 public class Tetromino {
+    public static final int[][][] SHAPES = {
+            // I
+            {{1, 1, 1, 1}},
+            // J
+            {{1, 0, 0}, {1, 1, 1}},
+            // L
+            {{0, 0, 1}, {1, 1, 1}},
+            // O
+            {{1, 1}, {1, 1}},
+            // S
+            {{0, 1, 1}, {1, 1, 0}},
+            // T
+            {{0, 1, 0}, {1, 1, 1}},
+            // Z
+            {{1, 1, 0}, {0, 1, 1}}
+    };
+
+    public int type;
+    public int rotation;
+    public int x, y;
+
+    public Tetromino(int type) {
+        this.type = type;
+        this.rotation = 0;
+        this.x = 3;
+        this.y = 0;
+    }
+
+    public int[][] getShape() {
+        return SHAPES[type];
+    }
 }
