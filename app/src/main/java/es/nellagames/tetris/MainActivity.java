8296d4962641e27abf0d2a74ff9cdeb5424984f3
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-06-19 00:44:12 +0200

    feat(audio): add background music to the game

--- app/src/main/java/es/nellagames/tetris/MainActivity.java
+++ app/src/main/java/es/nellagames/tetris/MainActivity.java
@@ -1,11 +1,14 @@
 package es.nellagames.tetris;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,22 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnLeft).setOnClickListener(v -> gameView.moveLeft());
         findViewById(R.id.btnRight).setOnClickListener(v -> gameView.moveRight());
         findViewById(R.id.btnRotate).setOnClickListener(v -> gameView.rotate());
+
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.tetris);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
+
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+
+}
 
 }
