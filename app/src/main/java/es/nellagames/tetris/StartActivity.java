0f7af1a007831789c3e59d135a9dc7e7d78d4d02
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-06-20 17:44:48 +0200

    feat(audio): add background music to StartActivity

--- app/src/main/java/es/nellagames/tetris/StartActivity.java
+++ app/src/main/java/es/nellagames/tetris/StartActivity.java
@@ -1,6 +1,7 @@
 package es.nellagames.tetris;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,15 +11,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class StartActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+
+        // Iniciar música de fondo
+        mediaPlayer = MediaPlayer.create(this, R.raw.tetris);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
         findViewById(R.id.btnStartGame).setOnClickListener(v -> {
             startActivity(new Intent(this, MainActivity.class));
             finish();
         });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
     }
 }
 
